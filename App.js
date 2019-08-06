a2bbc1540f0da8db68a79e8b3952f37d7f8b6159
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-08-06 16:12:13 +0300

    Implement Touches

--- App.js
+++ App.js
@@ -1,145 +1,107 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow
-//  */
+// Button Basics
+// import React, { Component } from "react";
+// import { Alert, AppRegistry, Button, StyleSheet, View } from "react-native";
 
-// import React, {Fragment} from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   ScrollView,
-//   View,
-//   Text,
-//   StatusBar,
-// } from 'react-native';
+// export default class ButtonBasics extends Component {
+//   _onPressButton() {
+//     Alert.alert("Stop touching me");
+//   }
 
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// const App = () => {
-//   return (
-//     <Fragment>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <Header />
-//           {global.HermesInternal == null ? null : (
-//             <View style={styles.engine}>
-//               <Text style={styles.footer}>Engine: Hermes</Text>
-//             </View>
-//           )}
-//           <View style={styles.body}>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Step One</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                 screen and then come back to see your edits.
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>See Your Changes</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <ReloadInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Debug</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <DebugInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Learn More</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Read the docs to discover what to do next:
-//               </Text>
-//             </View>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </Fragment>
-//   );
-// };
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.buttonContainer}>
+//           <Button onPress={this._onPressButton} title="Press me" />
+//         </View>
+//         <View style={styles.buttonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="Press Me"
+//             color="#841584"
+//           />
+//         </View>
+//         <View style={styles.alternativeLayoutButtonContainer}>
+//           <Button onPress={this._onPressButton} title="This looks great!" />
+//           <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
 
 // const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
+//   container: {
+//     flex: 1,
+//     justifyContent: "center"
 //   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
+//   buttonContainer: {
+//     margin: 20
 //   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
+//   alternativeLayoutButtonContainer: {
+//     margin: 20,
+//     flexDirection: "row",
+//     justifyContent: "space-between"
+//   }
 // });
 
-// export default App;
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent("AwesomeProject", () => ButtonBasics);
 
-// Button Basics
+//Touchables
 
-import React, { Component } from "react";
-import { Alert, AppRegistry, Button, StyleSheet, View } from "react-native";
+import React , { Component } from 'react';
+import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight,
+TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from
+'react-native';
 
-export default class ButtonBasics extends Component {
+export default class Touchables extends Component {
   _onPressButton() {
-    Alert.alert("Stop touching me");
+    Alert.alert('You taped the button!')
+  }
+
+  _onLongPressButton() {
+    Alert.alert('You long-pressed the button!')
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button onPress={this._onPressButton} title="Press me" />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button onPress={this._onPressButton} title="This looks great!" />
-          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
-        </View>
+      <View style={StyleSheet.container}>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+        <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback 
+          onPress={this._onPressButton} 
+          background={
+            Platform.OS === "android"
+              ? TouchableNativeFeedback.SelectableBackground()
+              : ""
+          }
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight
+          onPress={this._onPressButton}
+          onLongPress={this._onLongPressButton}
+          underlayColor="white"
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -147,18 +109,20 @@ export default class ButtonBasics extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center"
+    paddingTop: 60,
+    alignItems: 'center'
   },
-  buttonContainer: {
-    margin: 20
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: "row",
-    justifyContent: "space-between"
+  buttonText: {
+    padding: 20,
+    color: 'white'
   }
 });
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent("AwesomeProject", () => ButtonBasics);
+AppRegistry.registerComponent('AwesomeProject', () => Touchables);
