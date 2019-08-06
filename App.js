512f52a762b72ade8fa305ef417d6d9e890fa02e
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-08-06 14:42:45 +0300

    Finished Chapter

--- App.js
+++ App.js
@@ -1,114 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { View } from "react-native";
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default class FlexDirectionBasics extends Component {
+  render() {
+    return (
+      // Try setting `flexDirection` to `row`, `row-reverse`, `column`,
+      // `column-reverse`
+      //Try setting `justifyContent to `flex-start`, `center`, `flex-end`
+      // `space-between and `space-around`
+      //Try setting `alignItems` to `stretch`, `flex-start`, `flex-end`
+      // `center` and `baseline`
+      // Use alignSelf for a single child within it's parent
+      //Align content defines distribution of lines aling the cross axis with
+      // params simillar to justify content
+      //Flex wrap handles overflows outsidew the container
+      //FlexGrow describes how space within a cointainer should be distributed
+      // among children(integer)
+      //flexShrink how to shrink along main axis assuming the children overlflow
+      // the container(integer)
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "stretch",
+          flexGrow: 45,
+          position: "relative"
+        }}
+      >
+        <View style={{ width: 50, height: 50, backgroundColor: "red" }} />
+        <View style={{ width: 50, height: 50, backgroundColor: "yellow" }} />
+        <View style={{ width: 50, height: 50, backgroundColor: "green" }} />
+      </View>
+    );
+  }
+}
